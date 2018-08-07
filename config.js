4fec9f1af4becaa6714964e629595d70e60e5093
ridd <24556120+ridd84@users.noreply.github.com>
2018-08-07 23:39:59 +0200

    Update pools

--- config.js
+++ config.js
@@ -1,14 +1,12 @@
 var coin = "croat";
-var explorer = "http://178.22.71.122:46348";
+var explorer = "http://31.171.244.63:46348";
 var themeCss = "themes/nightly-mining-dark-theme.css";
 var networkStat = {
     "croat": [ 
-      ["croat.pool.cat", "https://croat.pool.cat:8119"], 
+      ["croat.pool.cat", "https://croat.pool.cat:2053"], 
       ["pool.croat.cat", "http://178.22.66.126:8117"],    
       ["pool.croatpirineus.cat", "https://pool.croatpirineus.cat:8119"],  
       ["croat.ebrepools.com", "https://croat.ebrepools.com:8119"],  
-      ["minar.mercatdelcroat.cat", "https://pool.mercatdelcroat.cat:8119"],  
       ["croatpool.com", "https://croatpool.com:8119"],  
-      ["croatbkk.hopto.org", "http://croatbkk.hopto.org:8000"],
     ]
 };
